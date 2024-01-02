808a7514c71f57b102f407bc125042708aae6669
7gaar <a7hmed2li@gmail.c>
2024-01-02 20:05:25 +0300

    d

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
-            c = chr(ord(c) - (ord('a') - ord('A')))
-        print("{:s}".format(c), end='')
-    print("")
+def print_last_digit(number):
+	print(abs(number) % 10, end="")
+	return (abs(number) % 10)

29fefe9a8441a0d54a0330d906c28de972ed0c82
7gaar <m3gamind10@gmail.com>
2024-02-09 19:09:02 +0300

    tt

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+contains the json string function.
+"""
+
+import json
+
+
+def from_json_string(my_str):
+	"""return an object"""
+	return json.loads(my_str)
---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+a function that writes an object to a file
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+	"""object to a text file using JSON"""
+	with open(filename, "w", encoding="utf-8") as f:
+		text = json.dumps(my_obj)
+		f.write(text)

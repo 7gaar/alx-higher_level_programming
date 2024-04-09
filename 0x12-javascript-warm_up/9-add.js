659c236d6184f7f3547d839c82f24d5e7c9c45fc
7gaar <m3gamind10@gmail.com>
2024-04-09 21:02:35 +0300

    f

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
-function factorial (n) {
-  if (n < 0) {
-    return (-1);
-  }
-  if (n === 0 || isNaN(n)) {
-    return (1);
-  }
-  return (n * factorial(n - 1));
+function add (a, b) {
+  const c = a + b;
+  console.log(c);
 }
 
-console.log(factorial(Number(process.argv[2])));
+add(Number(process.argv[2]), Number(process.argv[3]));
